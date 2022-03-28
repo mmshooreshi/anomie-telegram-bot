63a12a2c7eb54bec75d3344dd4f1de7c69f739c3
mmshooreshi <mmshooreshi@gmail.com>
2022-03-29 00:03:01 +0430

    bug

--- bot.rb
+++ bot.rb
@@ -88,12 +88,8 @@ Telegram::Bot::Client.run(token) do |bot|
       if message.text=="/start"
         reply_text = "سلام! خوش‌اومدی #{message.from.first_name}. 🤖. روی لینکی که داخل پیامت هست کلیک کن وگرنه پیامت رو فوروارد کن." 
       elsif "#{message.text.delete_prefix("/start ")}" == "#{codeVar}"
-        long_message_to_show =  " "
-        if message_orig == {}
-          long_message_to_show =  " "
-        elsif message_orig.key?(:"full_text")
-          long_message_to_show = message_orig["full_text"]
-        end
+        long_message_to_show = message_orig["full_text"]
+        
         reply_text = "پیام کامل که دنبالش بودی:
         ----
         #{long_message_to_show}"
