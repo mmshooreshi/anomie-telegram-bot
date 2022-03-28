d69729b60ae5a6cd4a88a6b7215f15778dd66258
mmshooreshi <mmshooreshi@gmail.com>
2022-03-29 01:11:56 +0430

    bug

--- bot.rb
+++ bot.rb
@@ -96,7 +96,7 @@ Telegram::Bot::Client.run(token) do |bot|
         begin
           if "#{message_orig}" != ""
             puts message_orig.keys 
-            long_message_to_show = message_orig.values[5]
+            long_message_to_show = message_orig.values[4]
           end
         rescue TypeError
           long_message_to_show=" "
