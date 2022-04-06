659fa1f78aadaad0049429af51ae04fdc2dbc029
mmshooreshi <mmshooreshi@gmail.com>
2022-04-07 02:28:42 +0430

    heeey

--- bot.rb
+++ bot.rb
@@ -138,7 +138,7 @@ def isTextMethod(message,bot)
       if $condom_protection == true
         bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "#{$reply_text}",protect_content: $condom_protection )
       else 
-        bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: " \# کلیک کن تا لینکت کپی شه و اون رو هر جایی دوست داری بفرست 
+        bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: " \\# کلیک کن تا لینکت کپی شه و اون رو هر جایی دوست داری بفرست 
            #{$reply_text}" , parse_mode: "MarkdownV2" )
       end
     end
