b95c5728e8bb03d4d500cd0221c00a6a7c3c4661
mmshooreshi <mmshooreshi@gmail.com>
2022-04-07 07:20:37 +0430

    CONDOMMMMM

--- bot.rb
+++ bot.rb
@@ -103,10 +103,10 @@ def isTextMethod(message,bot)
 #          $user_links_text="#{$user_links_text} `#{x[1]['full_text']}` [مشاهده](https://t.me/#{$bot_username}?start=#{Digest::MD5.hexdigest(x[1]['code'])[0...8]})
           if x[1]['timer']!=0
             $user_links_text="#{$user_links_text} 
-            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) | SEEN"
+            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) \| SEEN"
           else
             $user_links_text="#{$user_links_text}
-            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) | waiting..."
+            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) \| waiting..."
           end
           #puts $user_links_text
         end
