a8375b51731d4aafafbd42c7c39583f900598cfc
mmshooreshi <mmshooreshi@gmail.com>
2022-04-07 07:25:55 +0430

    emojis

--- bot.rb
+++ bot.rb
@@ -103,10 +103,10 @@ def isTextMethod(message,bot)
 #          $user_links_text="#{$user_links_text} `#{x[1]['full_text']}` [مشاهده](https://t.me/#{$bot_username}?start=#{Digest::MD5.hexdigest(x[1]['code'])[0...8]})
           if x[1]['timer']!=0
             $user_links_text="#{$user_links_text} 
-            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) :eight_spoked_asterisk: SEEN"
+            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) :sparkles: SEEN"
           else
             $user_links_text="#{$user_links_text}
-            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) :question: waiting..."
+            [#{x[1]['full_text'][0...50]}](https://t.me/#{$bot_username}?start=#{x[1]['code'][0...8]}) :question: :x: waiting..."
           end
           #puts $user_links_text
         end
